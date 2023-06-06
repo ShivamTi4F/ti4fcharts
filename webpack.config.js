5e3aa83971fc2c31afa73ddd5238f5064a40a2e7
shivamshayar <125987469+ShivamTi4F@users.noreply.github.com>
2023-06-06 23:33:21 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-react-package.js',
+    libraryTarget: 'umd',
+    library: 'MyReactPackage',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
