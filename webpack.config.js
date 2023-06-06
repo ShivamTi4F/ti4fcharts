996edd43b9ae15dd4966c0eaeb3cc8c2606482bb
shivamshayar <125987469+ShivamTi4F@users.noreply.github.com>
2023-06-07 00:23:23 +0200

    1.0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'my-react-package.js',
-    libraryTarget: 'umd',
-    library: 'MyReactPackage',
+  entry: {
+    main: './src/index.js',
+    donatChart: './src/charts/donatChart.js',
+    horizontalBarChart: './src/charts/horizontalBarChart.js',
+    pieChart: './src/charts/pieChart.js',
+    lineChart: './src/charts/lineChart.js',
+    verticalBarChart: './src/charts/verticalBarChart.js',
+    chartModel: './src/model/chartsModel.js',
+},
+output: {
+  path: path.resolve(__dirname, 'dist'),
+  filename: '[name].[contenthash].js',
+  libraryTarget: 'umd',
+    library: 'ti4fcharts',
   },
   module: {
     rules: [
