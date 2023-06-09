835b2fd41696de0f157268298d09fb2a40ef1988
shivamshayar <125987469+ShivamTi4F@users.noreply.github.com>
2023-06-09 18:07:45 +0200

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import DonatChart from './charts/donatChart';
+import PieChart from './charts/pieChart';
+import VerticalBarChart from './charts/verticalBarChart';
+import HorizontalBarChart from './charts/horizontalBarChart';
+import LineChart from './charts/lineChart';
 
-const App = () => {
-  return <div>Hello World !</div>;
+// const App = () => {
+//   return <div>Hello World !</div>;
+// };
+
+// export default App;
+
+export{
+  DonatChart, PieChart, VerticalBarChart, HorizontalBarChart, LineChart
 };
-
-export default App;
